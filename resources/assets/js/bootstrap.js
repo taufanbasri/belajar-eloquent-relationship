0f7d7017efc35bcd3a5c2ec0b5fb59a0f1fedeba
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-10 13:01:07 +0700

    add sweetalert2 package

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,7 +9,7 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-    // var $ = require( 'jquery' );
+    window.swal = require('sweetalert2');
     require( 'datatables.net' )( window, $ );
     require( 'datatables.net-bs' )( window, $ );
 
--- resources/views/datatables.blade.php
+++ resources/views/datatables.blade.php
@@ -1,9 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    {!! $html->table(['class' => 'table table-hover']) !!}
+    <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Datatables Example</h3>
+          </div>
+          <div class="panel-body">
+              {!! $html->table(['class' => 'table table-hover']) !!}
+          </div>
+        </div>
+
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">SweetAlert2 Example</h3>
+          </div>
+          <div class="panel-body">
+              <div class="wrap">
+                  <button class="btn btn-primary" onclick="sweet()">Click Me!</button>
+              </div>
+          </div>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
     {!! $html->scripts() !!}
+
+    <script>
+        function sweet() {
+            swal('Hello world!')
+        }
+    </script>
 @endsection
