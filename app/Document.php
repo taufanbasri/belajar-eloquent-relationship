3c532896f2c5f74c0fcf898a830dea2e4497d3ee
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-14 23:51:33 +0700

    Refactoring code Rercord Model Changes

--- app/Document.php
+++ app/Document.php
@@ -11,19 +11,34 @@ class Document extends Model
     {
         parent::boot();
 
-        static::updating(function ($document){
-           $document->adjustments()->attach(auth()->user()->id, [
-               'before' => json_encode(array_intersect_key($document->fresh()->toArray(), $document->getDirty())),
-               'after' => json_encode($document->getDirty())
-           ]);
+        static::updating(function ($document) {
+            $document->adjust();
         });
     }
 
     public function adjustments()
     {
         return $this->belongsToMany(User::class, 'adjustments')
-                ->withTimestamps()
-                ->withPivot(['before', 'after'])
-                ->latest('pivot_updated_at');
+            ->withTimestamps()
+            ->withPivot(['before', 'after'])
+            ->latest('pivot_updated_at');
+    }
+
+    public function adjust($userId = null, $diff = null)
+    {
+        $userId = $userId ?: Auth::id();
+        $diff = $diff ?: $this->getDiff();
+
+        return $this->adjustments()->attach($userId, $diff);
+    }
+
+    protected function getDiff()
+    {
+        $changed = $this->getDirty();
+
+        $before = json_encode(array_intersect_key($this->fresh()->toArray(), $changed));
+        $after = json_encode($changed);
+
+        return compact('before', 'after');
     }
 }
